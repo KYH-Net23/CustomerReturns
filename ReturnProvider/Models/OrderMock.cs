5a379466725bbcd3bd8a1cf4e8ff5a1a5e2a356a
Mille Elfver <mille.elfver98@gmail.com>
2024-11-19 13:18:14 +0100

    Add OrderMock and ReturnMock classes to Models namespace
    Two new classes, OrderMock and ReturnMock, have been added to the
    ReturnProvider.Models namespace. OrderMock includes properties for
    OrderId, ProductId, ProductName, Price, and OrderDate. ReturnMock
    includes properties for ReturnId, OrderId, Reason, Status, and Created.
    Reason and Status are initialized to non-null default values, and
    Created is initialized to the current date and time.

---
+++ ReturnProvider/Models/OrderMock.cs
@@ -0,0 +1,11 @@
+﻿namespace ReturnProvider.Models
+{
+    public class OrderMock
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int ProductName { get; set; }
+        public decimal Price { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
---
+++ ReturnProvider/Models/ReturnMock.cs
@@ -0,0 +1,11 @@
+﻿namespace ReturnProvider.Models
+{
+    public class ReturnMock
+    {
+        public int ReturnId { get; set; }
+        public int OrderId { get; set; }
+        public string Reason { get; set; } = null!;
+        public string Status { get; set; } = null!;
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
