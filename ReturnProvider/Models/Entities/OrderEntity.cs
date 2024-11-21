956387c520cfb374c1d88cf46f1de2292dcf6456
Mille Elfver <mille.elfver98@gmail.com>
2024-11-21 11:36:28 +0100

    Add OrderEntity class with properties and necessary usings
    - Added necessary `using` directives for `System` and `System.Collections.Generic`.
    - Introduced a new namespace `ReturnProvider.Models.Entities`.
    - Created a new class `OrderEntity` within the `ReturnProvider.Models.Entities` namespace.
    - Defined properties within the `OrderEntity` class:
      - `OrderId` of type `Guid`
      - `UserId` of type `Guid`
      - `IsReturnable` of type `bool`
      - `Items` of type `ICollection<OrderItemEntity>`

---
+++ ReturnProvider/Models/Entities/OrderEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReturnProvider.Models.Entities
+{
+    public class OrderEntity
+    {
+        public Guid OrderId { get; set; }
+        public Guid UserId { get; set; }
+        public bool IsReturnable { get; set; }
+        public ICollection<OrderItemEntity> Items { get; set; }
+    }
+}
