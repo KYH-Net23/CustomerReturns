ecbdd32d7734ee0d9c5809c3f5eeca5dee2df75f
Mille Elfver <mille.elfver98@gmail.com>
2024-11-21 11:37:14 +0100

    Add ReturnEntity class with properties in new file
    Created ReturnEntity.cs file.
    Imported System namespace.
    Defined ReturnProvider.Models.Entities namespace.
    Added ReturnEntity class with properties:
    - ReturnId (Guid)
    - OrderId (Guid)
    - UserId (Guid)
    - ReturnReason (string)
    - ResolutionType (string)
    - Status (string) with values: "Requested", "In Transit", "Refunded"
    - CreatedAt (DateTime)

---
+++ ReturnProvider/Models/Entities/ReturnEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReturnProvider.Models.Entities
+{
+    public class ReturnEntity
+    {
+        public Guid ReturnId { get; set; }
+        public Guid OrderId { get; set; }
+        public Guid UserId { get; set; }
+        public string ReturnReason { get; set; }
+        public string ResolutionType { get; set; }
+        public string Status { get; set; } // Requested, In Transit, Refunded
+        public DateTime CreatedAt { get; set; }
+    }
+}
