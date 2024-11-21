b92c7c6b6988e0afbb6168c75980af8e86f03746
Mille Elfver <mille.elfver98@gmail.com>
2024-11-21 11:39:00 +0100

    Add ReturnRequestModel class to ReturnProvider.Models
    A new class `ReturnRequestModel` has been created within the
    `ReturnProvider.Models` namespace. This class includes properties
    to hold information about a return request, such as the order ID,
    user ID, reason for the return, and the type of resolution desired.

---
+++ ReturnProvider/Models/ReturnRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ReturnProvider.Models
+{
+    public class ReturnRequestModel
+    {
+        public Guid OrderId { get; set; }
+        public Guid UserId { get; set; }
+        public string ReturnReason { get; set; }
+        public string ResolutionType { get; set; }
+    }
+
+}
