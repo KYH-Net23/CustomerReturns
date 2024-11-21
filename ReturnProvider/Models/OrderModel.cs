00bceacbbae0e198a14b41f45d989f23ec5cc965
Mille Elfver <mille.elfver98@gmail.com>
2024-11-21 11:38:27 +0100

    Add OrderModel class in ReturnProvider.Models namespace
    Introduced a new namespace `ReturnProvider.Models` and added a
    new class `OrderModel` within it. The `OrderModel` class includes
    the following properties: `OrderId` (Guid), `UserId` (Guid),
    `IsReturnable` (bool), and `Items` (List<OrderItemModel>).

---
+++ ReturnProvider/Models/OrderModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ReturnProvider.Models
+{
+    public class OrderModel
+    {
+        public Guid OrderId { get; set; }
+        public Guid UserId { get; set; }
+        public bool IsReturnable { get; set; }
+        public List<OrderItemModel> Items { get; set; }
+    }
+
+}
