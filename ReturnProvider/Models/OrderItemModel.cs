ddccb38a9132d6e664ea45cfde660a2ecface96a
Mille Elfver <mille.elfver98@gmail.com>
2024-11-21 11:38:10 +0100

    Add OrderItemModel class to ReturnProvider.Models namespace
    Introduce OrderItemModel class with properties ItemId, Name,
    Price, and Quantity to represent an order item within the
    ReturnProvider.Models namespace.

---
+++ ReturnProvider/Models/OrderItemModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ReturnProvider.Models
+{
+    public class OrderItemModel
+    {
+        public Guid ItemId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
