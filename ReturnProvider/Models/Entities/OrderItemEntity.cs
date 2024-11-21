4b2e0813ee309e8137d667cc20abd6802f00ba38
Mille Elfver <mille.elfver98@gmail.com>
2024-11-21 11:36:53 +0100

    Add OrderItemEntity class to ReturnProvider.Models.Entities
    A new namespace `ReturnProvider.Models.Entities` has been introduced.
    Within this namespace, a new class `OrderItemEntity` has been added.
    The `OrderItemEntity` class includes the following properties:
    - `ItemId` of type `Guid`
    - `OrderId` of type `Guid`
    - `Name` of type `string`
    - `Price` of type `decimal`
    - `Quantity` of type `int`
    The `System` namespace has been imported to support the use of the `Guid` type.

---
+++ ReturnProvider/Models/Entities/OrderItemEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ReturnProvider.Models.Entities
+{
+    public class OrderItemEntity
+    {
+        public Guid ItemId { get; set; }
+        public Guid OrderId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
