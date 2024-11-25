3ab36dbc8d28d4636e64b1c51566cb083369cbc8
Mille Elfver <mille.elfver98@gmail.com>
2024-11-25 14:01:47 +0100

    Enable Swagger in all environments, add ARM template
    Modified Swagger setup in `Program.cs` to make Swagger available in all environments by moving `app.UseSwagger()` outside the development check. Updated `app.UseSwaggerUI` configuration to set the Swagger endpoint and route prefix directly.
    
    Added `profile.arm.json` ARM template for deploying an Azure App Service. The template includes parameters for resource group name, location, and resource name, and defines variables for the App Service Plan name and resource ID. It specifies the creation of a resource group, an App Service Plan, and an App Service with HTTPS only enabled and a system-assigned identity.

--- ReturnProvider/Program.cs
+++ ReturnProvider/Program.cs
@@ -31,15 +31,13 @@ var app = builder.Build();
 app.UseCors("AllowAll");
 
 
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = string.Empty;
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
